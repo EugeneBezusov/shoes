61e1e6f9c291db12d8d0530a13b13ab3cf909386
EUGENE <EUGENEBEZUSOV@GMAIL.COM>
2019-02-19 12:29:31 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+    dots: true ,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+
